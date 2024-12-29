10d6ee9d1a11f814378bc740bfb5f9dd7d23b1e4
Khalid Bouychou <khbouych@e3r10p18.1337.ma>
2024-12-29 17:30:26 +0100

    client-panel

---
+++ client-panel/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App" style={
+      {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }
+    }>
+     <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
